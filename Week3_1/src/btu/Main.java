f66cca79cfa177ba736f0036d4bf297689f7674c
student <student@BTU.local>
2019-09-27 10:40:22 +0400

    My Second Commit in third week

--- Week3_1/src/btu/Main.java
+++ Week3_1/src/btu/Main.java
@@ -4,5 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        TestClass test1 = new TestClass();
+        test1.name = "Sandro";
+        test1.lastName = "Chkadua";
+        test1.setAge(20);
+        test1.printNameAndLastname();
+        System.out.println(test1.returnAgeAfterFourYear());
+
+
+        TestClass test2 = new TestClass();
+        test2.name = "Jeko";
+        test2.lastName = "Gobeglejiashvili";
+        test2.setAge(18);
+        test2.printNameAndLastname();
     }
 }
